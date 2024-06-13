0bba10918078723113347fca59dd3f763799e250
SantiagoGonzalez1206 <160767943+SantiagoGonzalez1206@users.noreply.github.com>
2024-06-13 09:09:26 -0500

    fix: :construction: js in index fixed and finished

--- js/components/menu.js
+++ js/components/menu.js
@@ -1,10 +1,10 @@
-export const menuListCategoryIndex = (res)=>{
+export const menuListCategoryIndex = async(res)=>{
     let {data} = res;
     let plantilla = "";
     data.forEach((value, index) => {
         plantilla += /*html*/`
         <li title="${value.name}">
-            <a href="#" >
+            <a href="?id=${value.id}" >
                 <img src="storage/img/category.svg" >
                 <span>${value.name}</span>
             </a>
