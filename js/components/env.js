37213446d6649fe2496cb899c2eacb3625e1918d
SantiagoGonzalez1206 <160767943+SantiagoGonzalez1206@users.noreply.github.com>
2024-06-25 08:26:13 -0500

    fix: :bug: key fix

--- js/components/env.js
+++ js/components/env.js
@@ -1,13 +1,4 @@
 export const headers = {
-<<<<<<< HEAD
-    'x-rapidapi-key': '3a8f131c72msh6206d6e808ff5e2p11211fjsne419179b9916',
+    'x-rapidapi-key': '9e4a2e5312mshb87343eaa1736bfp1b9a9fjsn96ec10faea12',
     'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
-=======
-	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com',
-<<<<<<< HEAD
-	'x-rapidapi-key': 'c1dc326a99msh3e1722fa4126b7ep13497djsnae21f057554a'
->>>>>>> f9bbe3c (fix: :construction: adventages in button of quantity)
-=======
-	'x-rapidapi-key': '9e4a2e5312mshb87343eaa1736bfp1b9a9fjsn96ec10faea12'
->>>>>>> c6dd43f (fix: :bug: bug in checkout fixed)
 }
